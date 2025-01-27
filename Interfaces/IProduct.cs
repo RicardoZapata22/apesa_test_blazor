61ca4beac81e551b69646f3b32739f64e99aea1e
RicharZapata <ricardo.zapata@ardana.com.mx>
2025-01-27 14:54:35 -0600

    Feature: Se crea clase para gestion de productos, se crean estilos para tabla y registros, se implementa modal para dar de alta registro

---
+++ Interfaces/IProduct.cs
@@ -0,0 +1,14 @@
+namespace apesa_test.Interfaces{
+    public class Product {
+        public string name {get; set; }
+        public string type {get; set; }
+        public double interest_rate {get; set; } = 0;
+        public Product(string name, string type, double interest_rate = 0.0){
+            this.name = name;
+            this.type = type;
+            this.interest_rate = interest_rate;
+        }
+
+
+    }
+}
