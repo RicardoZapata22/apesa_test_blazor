be51fff885779a929af0c224282954d644f45f38
RicharZapata <ricardo.zapata@ardana.com.mx>
2025-01-27 20:22:21 -0600

    Ajustes para implementar funcionalidad para nuevo registro

--- Interfaces/IProduct.cs
+++ Interfaces/IProduct.cs
@@ -3,7 +3,7 @@ namespace apesa_test.Interfaces{
         public string name {get; set; }
         public string type {get; set; }
         public double interest_rate {get; set; } = 0;
-        public Product(string name, string type, double interest_rate = 0.0){
+        public Product(string name = "", string type = "", double interest_rate = 0.0){
             this.name = name;
             this.type = type;
             this.interest_rate = interest_rate;
