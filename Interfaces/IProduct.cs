a421a5b929a756e5c85f4c12e3fc68af52c4490d
RicharZapata <ricardo.zapata@ardana.com.mx>
2025-01-29 00:59:52 -0600

    Se actualiza interfaz de producto para auto set al cargar json

--- Interfaces/IProduct.cs
+++ Interfaces/IProduct.cs
@@ -1,14 +1,24 @@
-namespace apesa_test.Interfaces{
-    public class Product {
-        public string name {get; set; }
-        public string type {get; set; }
-        public double interest_rate {get; set; } = 0;
-        public Product(string name = "", string type = "", double interest_rate = 0.0){
-            this.name = name;
-            this.type = type;
-            this.interest_rate = interest_rate;
-        }
+using System.Text.Json.Serialization;
 
+namespace apesa_test.Interfaces{
+
+    public class IProduct: ICloneable {
+        [JsonPropertyName("product_id")]
+        public string? ProductId { get; set; } = "";
+
+        [JsonPropertyName("name")]
+        public string? Name {get; set; }
+
+        [JsonPropertyName("product_type")]
+        public string? ProductType {get; set; }
+
+        [JsonPropertyName("interest_rate")]
+        public double InterestRate {get; set; } = 0;
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
 
     }
 }
